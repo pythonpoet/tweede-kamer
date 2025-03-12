39663fdcabc4ff0aa39056c61720a0c906279962
David Wild <david_wild@gmx.ch>
2025-03-12 12:34:14 +0100

    add llm.py

---
+++ llm.py
@@ -0,0 +1,9 @@
+class LLM(Self):
+    def init(self, model):
+        # specify which model to run eg. deepseek:8b
+        # TODO initialise modal
+        pass
+    
+    def makeCompute(self):
+        # TODO run a llm query
+        pass
