14cfe194de1cf0b2078d33d42fa141ffaccf3ac5
David Wild <david_wild@gmx.ch>
2025-03-21 12:20:37 +0100

    testing small version

--- shell.nix
+++ shell.nix
@@ -8,6 +8,7 @@ pkgs.mkShell {
     pkgs.python3Packages.deep-translator
     pkgs.python3Packages.tqdm
     pkgs.python3Packages.nltk
+    pkgs.python3Packages.pysocks
     pkgs.gcc
     pkgs.zlib
     # Include any other dependencies you might need
